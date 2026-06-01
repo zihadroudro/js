eb3f61da1bf7044175789691d79f8484d6e42666
zihadroudro <zihadroudro01@gmail.com>
2026-06-01 08:50:58 +0000

    variable config'

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const account_id=144553
+let account_email="zihad@google.com"
+let account_password="12345"
+account_city="Sherpur"
+let account_state;
+
+//account_id=2 //not allowed
+
+account_email='jcjc@.com'
+account_password='222'
+account_city='lolo'
+
+
+console.log(account_id);
+
+/*
+prefer not to use var because of issue in block scope and functioanla scope
+
+*/
+
+console.table([account_id,account_email,account_password,account_city,account_state])
