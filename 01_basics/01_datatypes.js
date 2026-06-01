3ba94fe10c8dcd247264d5c87142a2432353900d
zihadroudro <zihadroudro01@gmail.com>
2026-06-01 11:37:19 +0000

    datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -2,3 +2,19 @@
 
 //alert("hello") wre using node js not browser
 console.log(3+3)
+
+let name='h'
+let age=22
+let isloggedIn=false
+let sate=null
+
+//number=> 2^53
+// bigint
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+// undefined=>
+//symbol=>unique
+//object
+
+console.log(typeof null)
